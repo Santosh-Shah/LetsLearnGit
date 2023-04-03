f0d776aa5c27b7c6c48966c7b14054e2ef9a910a
Santosh Shah <santoshofficial679@gmail.com>
2023-04-03 21:04:18 +0545

    Sysout.out.println function is added

--- Diff.java
+++ Diff.java
@@ -3,5 +3,6 @@ public class Diff {
         int a = 5;
         int b = 6;
         System.out.println("Substraction of two value: " + a + b);
+        System.out.println("This is Substraction");
     }
 }
